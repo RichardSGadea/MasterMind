26d491f9498cb10fd1b1f1b7fb2bb6573db528be
Richard <richardsgadea@gmail.com>
2024-02-05 18:39:53 +0100

    Added colors selected in game.html with javascript (game.js)

--- js/game.js
+++ js/game.js
@@ -1,8 +1,25 @@
 let nickName = localStorage.getItem("nickName");
 let localColor = localStorage.getItem("colorsPlay");
-let trieds = localStorage.getItem("tried");
+let attempts = localStorage.getItem("tried");
 /*
 console.log(nickName);
 console.log(localColor);
-console.log(trieds);
+console.log(attempts);
 */
+//console.log(typeof localColor); //string
+let boxColorsSelected = localColor.split(',');
+//console.log(boxColorsSelected); //[color1,color2,color3,color4,.....] 
+let colorsSelected = document.getElementById("colors");
+document.addEventListener("DOMContentLoaded", ()=>{
+    for(let i = 0;i<boxColorsSelected.length;i++){
+        let boxColor = document.createElement("div");
+        boxColor.id = `color${i}`;
+        boxColor.className = "buttonColorSelected";
+        boxColor.style.backgroundColor=`${boxColorsSelected[i]}`;
+        colorsSelected.appendChild(boxColor);
+    }
+})
+
+
+
+
