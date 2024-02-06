ddbf37dbe9f8dc81f263503ded2b4c7eded87058
Richard <richardsgadea@gmail.com>
2024-02-06 09:12:40 +0100

    Added game board depending of the difficulty selected

--- js/game.js
+++ js/game.js
@@ -8,8 +8,11 @@ console.log(attempts);
 */
 //console.log(typeof localColor); //string
 let boxColorsSelected = localColor.split(',');
+//console.log(typeof parseInt(attempts)); //number
 //console.log(boxColorsSelected); //[color1,color2,color3,color4,.....] 
 let colorsSelected = document.getElementById("colors");
+let colorsAttempts = document.getElementById("masterMind");
+let attemptBox=[];
 document.addEventListener("DOMContentLoaded", ()=>{
     for(let i = 0;i<boxColorsSelected.length;i++){
         let boxColor = document.createElement("div");
@@ -18,7 +21,25 @@ document.addEventListener("DOMContentLoaded", ()=>{
         boxColor.style.backgroundColor=`${boxColorsSelected[i]}`;
         colorsSelected.appendChild(boxColor);
     }
+    for(let i = 0; i<parseInt(attempts);i++){
+        let boxAttempt = document.createElement("div");
+        boxAttempt.id = `attempt${i}`;
+        boxAttempt.className = "boxesAttempts";
+        //let attempt = document.createElement("span");
+        //let numberAttempt = document.createTextNode(`${i+1}.`);
+        //attempt.appendChild(numberAttempt);
+        //boxAttempt.appendChild(attempt);
+        colorsAttempts.appendChild(boxAttempt);
+        for(let i = 0;i<boxColorsSelected.length;i++){
+            let boxColorAttempts = document.createElement("div");
+            boxColorAttempts.id=`color${i}`;
+            boxColorAttempts.className = "ColorsAttemptBox";
+            boxAttempt.appendChild(boxColorAttempts);
+        }
+    }
 })
+
+
 
 
 
